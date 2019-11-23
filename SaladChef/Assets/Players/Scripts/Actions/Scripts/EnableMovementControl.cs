5a0a2410dee8d55969027f50d56b802cfb0bb696
Abhibhu <sonkar.abhi86@gmail.com>
2019-11-24 03:59:12 +0530

    Bug fix chef movement
    disable movement during the choppin state and then re-enable it.
    Also adjusted the collider for the vegitable

---
+++ SaladChef/Assets/Players/Scripts/Actions/Scripts/EnableMovementControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SaladChef
+{
+    [CreateAssetMenu(menuName = "Player/Action/EnableDisableMovemenControl")]
+    public class EnableMovementControl : ChefAction
+    {
+        [SerializeField] private bool m_EnableMovementControl;
+
+        public override void DoAction()
+        {
+            m_ChefTransform.value.GetComponent<MovementController>().enabled = m_EnableMovementControl;
+        }
+
+        public override void DoActionUpdate(float deltaTime)
+        {
+        }
+    }
+}
